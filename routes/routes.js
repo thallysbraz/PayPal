41986f0954303066c17289e282d53bd0e616a85a
thallysbraz <thallysbraz3@gmail.com>
2020-03-12 17:27:24 -0300

    criando plano de assinatura

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const routes = express.Router();
 
 const PaymentController = require("../src/Controllers/PaymentController");
+const PlanController = require("../src/Controllers/PlanController");
 
 routes.get("/", (req, res) => {
   res.render("index.ejs");
@@ -13,5 +14,9 @@ routes.post("/comprar", PaymentController.store);
 
 //rota para processar o pagamento
 routes.get("/final", PaymentController.confirmStore);
+
+//rota para criar um plano
+
+routes.get("/plancreate", PlanController.store);
 
 module.exports = routes;
